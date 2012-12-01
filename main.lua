e27264193f57a9fea451d1071dc82597a60dfb9b
David Gomes <davidrafagomes@gmail.com>
2012-12-01 12:56:37 +0000

    Changed position of nextBlock.

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@
 ]]--
 
 function love.load()
-  -- love.physics.setMeter(64)
+  currentState = "play"
   world = love.physics.newWorld(0, 9.8 * 64, true)
   world:setCallbacks(beginContact, nil, nil, nil)
   numberOfCollisions = 0
@@ -38,78 +38,83 @@ function love.load()
 end
 
 function love.update(dt)
-  world:update(dt)
+  if currentState == "play" then
+    world:update(dt)
 
-  -- Add new blocks
-  if love.mouse.isDown("l") then
-    if canAddBlock then
-      local block = {}
-      block.body = love.physics.newBody(world, love.mouse.getX(), love.mouse.getY(), "dynamic")
-      block.shape = love.physics.newRectangleShape(0, 0, newBlock.width, newBlock.height)
-      block.height = newBlock.height
-      block.fixture = love.physics.newFixture(block.body, block.shape, 100)
-      block.fixture:setRestitution(0)
+    -- Add new blocks
+    if love.mouse.isDown("l") then
+      if canAddBlock then
+        local block = {}
+        block.body = love.physics.newBody(world, love.mouse.getX(), love.mouse.getY(), "dynamic")
+        block.shape = love.physics.newRectangleShape(0, 0, newBlock.width, newBlock.height)
+        block.height = newBlock.height
+        block.fixture = love.physics.newFixture(block.body, block.shape, 100)
+        block.fixture:setRestitution(0)
 
-      newBlock.width, newBlock.height = nextBlock.width, nextBlock.height
+        newBlock.width, newBlock.height = nextBlock.width, nextBlock.height
 
-      nextBlock.width = math.random(5, 100)
-      nextBlock.height = math.random(5, 30)
+        nextBlock.width = math.random(5, 100)
+        nextBlock.height = math.random(5, 30)
 
-      table.insert(blocks, block)
-      canAddBlock = false
+        table.insert(blocks, block)
+        canAddBlock = false
+      end
+    else
+      canAddBlock = true
     end
-  else
-    canAddBlock = true
-  end
 
-  -- Check if player lost
-  if numberOfCollisions > 1 then
-    love.filesystem.write("data", tostring(maxScore), 2)
-    love.load()
-  end
+    -- Check if player lost
+    if numberOfCollisions > 1 then
+      love.filesystem.write("data", tostring(maxScore), 2)
+      love.load()
+    end
 
-  -- Update maximum score
-  if #blocks > maxScore then
-    maxScore = #blocks
-  end
+    -- Update maximum score
+    if #blocks > maxScore then
+      maxScore = #blocks
+    end
 
-  -- Restart game
-  if love.keyboard.isDown("r") then
-    love.load()
-  end
+    -- Restart game
+    if love.keyboard.isDown("r") then
+      love.load()
+    end
 
-  -- Exit the game
-  if love.keyboard.isDown("escape") then
-    love.event.quit()
+    -- Exit the game
+    if love.keyboard.isDown("escape") then
+      love.event.quit()
+    end
   end
 end
 
 function love.draw()
-  -- Draw the ground
-  love.graphics.setColor(72, 160, 14)
-  love.graphics.polygon("fill", ground.body:getWorldPoints(ground.shape:getPoints()))
+  if currentState == "play" then
+    -- Draw the ground
+    love.graphics.setColor(72, 160, 14)
+    love.graphics.polygon("fill", ground.body:getWorldPoints(ground.shape:getPoints()))
 
-  -- Draw the blocks
-  love.graphics.setColor(50, 50, 50)
-  for i = 1, #blocks do
-    love.graphics.polygon("fill", blocks[i].body:getWorldPoints(blocks[i].shape:getPoints()))
-  end
+    -- Draw the blocks
+    love.graphics.setColor(50, 50, 50)
+    for i = 1, #blocks do
+      love.graphics.polygon("fill", blocks[i].body:getWorldPoints(blocks[i].shape:getPoints()))
+    end
 
-  -- Draw block
-  love.graphics.setColor(255, 240, 240, 50)
-  love.graphics.rectangle("fill", love.mouse.getX() - newBlock.width / 2, love.mouse.getY() - newBlock.height / 2, newBlock.width, newBlock.height)
+    -- Draw block
+    love.graphics.setColor(255, 240, 240, 70)
+    love.graphics.rectangle("fill", love.mouse.getX() - newBlock.width / 2, love.mouse.getY() - newBlock.height / 2, newBlock.width, newBlock.height)
 
-  -- Draw next block
-  love.graphics.rectangle("fill", 10, 70, nextBlock.width, nextBlock.height)
+    -- Draw next block
+    love.graphics.setColor(255, 240, 240, 15)
+    love.graphics.rectangle("fill", love.mouse.getX() - nextBlock.width / 2, love.mouse.getY()- nextBlock.height / 2 - 100, nextBlock.width, nextBlock.height)
 
-  -- Draw the score
-  love.graphics.setColor(0, 0, 0)
-  love.graphics.print(#blocks, 10, 8)
-  love.graphics.print(maxScore, 10, 35)
+    -- Draw the score
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print(#blocks, 10, 8)
+    love.graphics.print(maxScore, 10, 35)
 
-  -- Draw welcome message
-  if #blocks == 0 then
-    love.graphics.print("Left click to insert a block.\nTry to make a huge tower.", 200, 400)
+    -- Draw welcome message
+    if #blocks == 0 then
+      love.graphics.print("Left click to insert a block.\nTry to make a huge tower.", 200, 400)
+    end
   end
 end
 
